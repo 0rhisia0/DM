c33cac146a9b7075c005389e664a8f7613b2da12
0rhisia0 <ishira.fernando00@gmail.com>
2020-05-29 10:57:00 -0400

    Initial Commit

--- venv/MCMC_Model.py
+++ venv/MCMC_Model.py
@@ -11,7 +11,7 @@ M_D_Low = 1*const.GeV
 M_D_High = 300*const.GeV
 sigma_high = 1e-43*const.cm2
 sigma_low = 1e-47*const.cm2
-N = 100000  # number of markov chain transitions
+N = 100  # number of markov chain transitions
 
 
 def proposal(WIMP_curr):
@@ -19,7 +19,6 @@ def proposal(WIMP_curr):
     curr_mass = WIMP_curr[0]
     curr_sigma = WIMP_curr[1]
     M_D_new = np.random.normal(curr_mass, 100*const.GeV)  # sample param for mass
-    # sigma_new = np.random.normal(curr_sigma, 1e-47)
     sigma_new = lognorm.rvs(1, scale=curr_sigma, loc=1e-48*const.cm2)  # sample param for cross section
     M_D_new = np.clip(M_D_new, M_D_Low, M_D_High)
     sigma_new = np.clip(sigma_new, sigma_low, sigma_high)
@@ -39,11 +38,12 @@ def main():
     # plt.show()
     """
     # define events
-    events = [0]
+    events = [15*const.keV, 15*const.keV, 15*const.keV, 15*const.keV, 15*const.keV, 15*const.keV]
     # pick start
-    theta = np.zeros(2)
+    theta = np.zeros((N, 2))
     # initialize MCMC parameters
     theta[0] = [const.M_D, const.sigma]
+    #Metropolis Hastings loop
     for i in range(1, N):
         proposed_theta = proposal(theta[i-1])
         numer = lik.events_likelihood(0.001*const.keV, 200*const.keV, 1000,
@@ -51,10 +51,12 @@ def main():
         denom = lik.events_likelihood(0.001*const.keV, 200*const.keV, 1000,
                                   events, proposed_theta, const.AXe, 10*const.keV)
         ratio = numer/denom
-        comp = np.random(0, 1)
+        comp = np.random.uniform(0, 1)
         if comp >= np.min([1, ratio]):
+            theta[i] = proposed_theta
+        else:
             theta[i] = theta[i-1]
-
+    print(theta)
 
 if __name__ == "__main__":
     main()
